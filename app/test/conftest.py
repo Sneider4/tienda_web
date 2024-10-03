a5435e14fa6f76ddee9d1541c93ae19366b34b82
Sneider4 <163562678+Sneider4@users.noreply.github.com>
2024-10-03 07:34:57 -0500

    test login

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -1,23 +1,35 @@
 from app import create_app, db
 import pytest
+from app.models.usuario import Usuario
 
 @pytest.fixture
 def app():
     app = create_app()
+    app.config['WTF_CSRF_ENABLED'] = False
     with app.app_context():
         db.create_all()
         yield app
         db.session.remove()
-        #db.drop_all()
 
 @pytest.fixture
 def client(app):
-    return app.text_client()
+    return app.test_client()
 
 @pytest.fixture
-def user(app):
-    from app.models.usuario import Usuario
-    usuario = Usuario(nombre="test_user", contrasena="test_contrasena" )
+def usuario(app):
+    usuario = Usuario(
+        nombre="test_user", 
+        apellido="test_apellido",
+        correo_electronico="test_user@example.com",
+        telefono="test_telefono",
+        contrasena="test_contrasena",
+        departamento="test_departamento",
+        ciudad="test_ciudad",
+        genero="test_genero",
+        fecha_nacimiento="2000-01-01",
+        imagen="test_imagen"
+    )
     db.session.add(usuario)
-    db.session.commit
+    db.session.commit()
     yield usuario
+    db.session.delete(usuario)
